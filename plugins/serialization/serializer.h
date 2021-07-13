b7e8b63b461914091a8c30eea1a1895374fadfb4
Dmitriy Merkushov <d.merkushov@gmail.com>
2021-07-14 00:51:14 +0300

    WIP: serializer.h: current state

--- plugins/serialization/serializer.h
+++ plugins/serialization/serializer.h
@@ -6,15 +6,37 @@
 #define NANOSERVICES2_MARSHALLER_H
 
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace nanoservices {
 
-template<typename T>
-concept Serializable = requires(T t) {
-    { t.fieldGetterFunction() } -> std::function<std::vector<byte>(std::string)>;
-    { t.fieldSetterFunction() } -> std::function<void(std::string, std::vector<byte>)>;
+enum class RecordType {
+    NULL_VALUE,
+    BYTE_ARRAY,
+    STRING,
+    SIGNED_INT_8,
+    UNSIGNED_INT_8,
+    SIGNED_INT_16,
+    UNSIGNED_INT_16,
+    SIGNED_INT_32,
+    UNSIGNED_INT_32,
+    SIGNED_INT_64,
+    UNSIGNED_INT_64,
+    FLOAT,
+    DOUBLE,
+    RECORD_LIST,
+    RECORD_MAP
 };
+
+template<typename T>
+struct TypedRecord {
+    RecordType type;
+    std::string name;
+    std::shared_ptr<T> record;
+};
+
+// TODO Serializable concept
 
 template<Serializable S>
 class SerializedEntity {
