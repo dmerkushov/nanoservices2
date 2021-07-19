e4920ae09889814a8f6c0638d706770c3e984f06
Dmitriy Merkushov <d.merkushov@gmail.com>
2021-07-20 01:09:23 +0300

    WIP: serializer.h: current state

--- plugins/serialization/serializer.h
+++ plugins/serialization/serializer.h
@@ -27,9 +27,8 @@ enum class RecordType {
     UNSIGNED_INT_32,
     SIGNED_INT_64,
     UNSIGNED_INT_64,
-    FLOAT,
-    DOUBLE,
-    ENUM_VALUE,
+    FLOAT_32,
+    FLOAT_64,
     LIST,
     MAP
 };
@@ -90,10 +89,73 @@ public:
     template<typename T>
     static std::shared_ptr<SerializerRecord> serializeField(std::shared_ptr<std::string> fieldName, T &fieldValue) {
         auto record = std::make_shared<SerializerRecord>();
-        if constexpr(std::is_same_v<T, int32_t>) {
+        record->fieldName = fieldName;
+        if constexpr(std::is_same_v<T, std::string>) {
+            record->type = RecordType::STRING;
+            record->data = std::make_shared<std::string>(fieldValue);
+        } else if constexpr(std::is_same_v<T, std::shared_ptr<std::string>>) {
+            record->type = RecordType::STRING;
+            record->data = fieldValue;
+        } else if constexpr(std::is_same_v<T, int8_t>) {
+            record->type = RecordType::SIGNED_INT_8;
+            record->data = std::make_shared<int8_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, u_int8_t>) {
+            record->type = RecordType::UNSIGNED_INT_8;
+            record->data = std::make_shared<u_int8_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, int16_t>) {
+            record->type = RecordType::SIGNED_INT_16;
+            record->data = std::make_shared<int16_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, u_int16_t>) {
+            record->type = RecordType::UNSIGNED_INT_16;
+            record->data = std::make_shared<u_int16_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, int32_t>) {
             record->type = RecordType::SIGNED_INT_32;
-            record->fieldName = fieldName;
             record->data = std::make_shared<int32_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, u_int32_t>) {
+            record->type = RecordType::UNSIGNED_INT_32;
+            record->data = std::make_shared<u_int32_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, int64_t>) {
+            record->type = RecordType::SIGNED_INT_64;
+            record->data = std::make_shared<int64_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, u_int64_t>) {
+            record->type = RecordType::UNSIGNED_INT_64;
+            record->data = std::make_shared<u_int64_t>(fieldValue);
+        } else if constexpr(std::is_same_v<T, float>) {
+            record->type = RecordType::FLOAT_32;
+            record->data = std::make_shared<float>(fieldValue);
+        } else if constexpr(std::is_same_v<T, double>) {
+            record->type = RecordType::FLOAT_64;
+            record->data = std::make_shared<double>(fieldValue);
+        } else if constexpr(std::is_same_v<T, std::vector<>>) {
+            record->type = RecordType::FLOAT_64;
+            record->data = std::make_shared<double>(fieldValue);
+        } else {
+            record->type = RecordType::NULL_VALUE;
+        }
+        return record;
+    }
+
+    template<typename T>
+    static std::shared_ptr<SerializerRecord> serializeListField(std::shared_ptr<std::string> fieldName,
+                                                                std::vector<T> &fieldValue) {
+        auto record = std::make_shared<SerializerRecord>();
+        record->fieldName = fieldName;
+        if constexpr(std::is_same_v<T, std::vector<char>>) {
+            record->type = RecordType::BYTE_ARRAY;
+            record->data = std::make_shared<std::vector<char>>(fieldValue);
+        } else if constexpr(std::is_same_v<T, std::shared_ptr<std::vector<char>>>) {
+            record->type = RecordType::BYTE_ARRAY;
+            record->data = fieldValue;
+        } else {
+            record->type = RecordType::LIST;
+            record->data = std::make_shared<std::vector<std::shared_ptr<SerializerRecord>>>();
+
+            auto data = std::static_pointer_cast<std::vector<std::shared_ptr<SerializerRecord>>>(record->data);
+            auto allNamesEmpty = std::make_shared<std::string>("");
+
+            for(auto iter = fieldValue.begin(); iter != fieldValue.end(); ++iter) {
+                data->push_back(serializeField(allNamesEmpty, fieldValue));
+            }
         }
         return record;
     }
