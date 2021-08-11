7116189b4f785831e2b6f24408bd8572531b1455
Dmitriy Merkushov <d.merkushov@gmail.com>
2021-08-11 23:36:59 +0300

    position.h: include <string>

--- util/macroutils/position.h
+++ util/macroutils/position.h
@@ -5,8 +5,9 @@
 #ifndef NANOSERVICES2_POSITION_H
 #define NANOSERVICES2_POSITION_H
 
+#include <string>
+
 // Sonar advises using std::source_location of C++20, but GCC 10.3.0 doesn't support it
-#define NS_POSITION \
-    ((std::string("") + __FILE__ + ":" + std::to_string(__LINE__) + ", in " + __PRETTY_FUNCTION__).c_str())
+#define NS_POSITION ((std::string("") + __FILE__ + ":" + std::to_string(__LINE__) + ", in " + __PRETTY_FUNCTION__).c_str())
 
 #endif // NANOSERVICES2_POSITION_H
