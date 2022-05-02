abcc754e2c8e70b45464f97382c2c7761916b0d0
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-05-02 21:41:55 +0300

    Check if Sonar can see the test suite

---
+++ core/exception/exception_test.cpp
@@ -0,0 +1,12 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../../plugins/logging/logging.h"
+#include "../../thirdparty/doctest/doctest/doctest.h"
+#include "ns_exception.h"
+
+using namespace std;
+using namespace nanoservices;
+
+TEST_CASE("ns_exception ctor") {
+    ns_exception e("My exception", NS_POSITION_SHAREDPTR);
+    log::critical("{}", e);
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "core/serialization/serialization.h"
 #include "plugins/configuration/configuration.h"
 #include "plugins/logging/logging.h"
+#include "thirdparty/doctest/doctest/doctest.h"
 #include "thirdparty/gabime/spdlog/fmt/bin_to_hex.h"
 #include "thirdparty/gabime/spdlog/spdlog.h"
 
@@ -34,6 +35,15 @@ struct MyEnclosingClass {
 int main(int argc, char **argv) {
     configuration::initialize(argc, argv);
     logging_initialize();
+
+    if(configuration::get_property("nanoservices.testing.nstests.atstartup")) {
+        doctest::Context context;
+        context.applyCommandLine(argc, argv);
+        int doctest_result = context.run();
+        if(context.shouldExit()) {
+            return doctest_result;
+        }
+    }
 
     ns_exception e2(make_shared<ns_exception>("Message1", NS_POSITION_SHAREDPTR), make_shared<string>("Message2"), NS_POSITION_SHAREDPTR);
 
