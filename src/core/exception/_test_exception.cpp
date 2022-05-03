45d60ca3f6904d35b1eed3f9ce1f7c8f689e9a53
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-05-04 02:06:02 +0300

    _test_exception.cpp: improve test_ns_exception_ctor

--- src/core/exception/_test_exception.cpp
+++ src/core/exception/_test_exception.cpp
@@ -6,12 +6,19 @@
 using namespace std;
 using namespace nanoservices;
 
-function<bool()> test_ns_exception_ctor = []() {
-    auto e1 = make_shared<ns_exception>("My exception", NS_POSITION_SHAREDPTR);
+static function<bool()> test_ns_exception_ctor = []() {
+    auto e1 = make_shared<ns_exception>("My exception 1", NS_POSITION_SHAREDPTR);
     auto e2 = make_shared<ns_exception>(e1, make_shared<string>("My exception 2"), NS_POSITION_SHAREDPTR);
 
-    regex re(".*My exception.*");
-    return regex_match(*e1->whatstr(), re);
+    regex re1(".*My exception 1.*");
+    regex re2(".*My exception 2.*");
+    return
+            // Testing whatstr for the first exception
+            regex_match(*e1->whatstr(), re1) &&
+            !regex_match(*e1->whatstr(), re2)
+
+            // Testing whatstr for the second exception (with a cause)
+            && regex_match(*e2->whatstr(), re1) && regex_match(*e2->whatstr(), re2);
 };
 
 NSTEST_ADD(test_ns_exception_ctor)
