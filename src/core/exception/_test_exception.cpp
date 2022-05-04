17a399c7f26f9938bf614fe773f81d9746319b75
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-05-04 19:39:14 +0300

    _test_exception.cpp: fix test_ns_exception

--- src/core/exception/_test_exception.cpp
+++ src/core/exception/_test_exception.cpp
@@ -1,3 +1,4 @@
+#include "../../plugins/logging/logging.h"
 #include "../../util/testing/testing.h"
 #include "ns_exception.h"
 
@@ -6,19 +7,32 @@
 using namespace std;
 using namespace nanoservices;
 
-static const function<bool()> test_ns_exception_ctor = []() {
+static const function<bool()> test_ns_exception = []() {
     auto e1 = make_shared<ns_exception>("My exception 1", NS_POSITION_SHAREDPTR);
     auto e2 = make_shared<ns_exception>(e1, make_shared<string>("My exception 2"), NS_POSITION_SHAREDPTR);
 
-    regex re1(".*My exception 1.*");
-    regex re2(".*My exception 2.*");
-    return
-            // Testing whatstr for the first exception (without a cause, so re2 should not match)
-            regex_match(*e1->whatstr(), re1) &&
-            !regex_match(*e1->whatstr(), re2)
+    regex re1(".*My exception 1", std::regex::extended);
+    regex re2(".*My exception 2\n.*");
 
-            // Testing whatstr for the second exception (with a cause)
-            && regex_match(*e2->whatstr(), re1) && regex_match(*e2->whatstr(), re2);
+    bool result = true;
+
+    if(!regex_match(*(e1->whatstr()), re1)) {
+        log::warn("e1->whatstr() : \"{}\" doesn't match regular expression re1", e1->whatstr());
+        result = false;
+    }
+    if(regex_match(*(e1->whatstr()), re2)) {
+        log::warn("e1->whatstr() : \"{}\" matches regular expression re1", e1->whatstr());
+        result = false;
+    }
+    if(!regex_match(*(e2->whatstr()), re1)) {
+        log::warn("e2->whatstr() : \"{}\" doesn't match regular expression re1", e2->whatstr());
+        result = false;
+    }
+    if(!regex_match(*(e2->whatstr()), re2)) {
+        log::warn("e2->whatstr() : \"{}\" doesn't match regular expression re2", e2->whatstr());
+        result = false;
+    }
+    return result;
 };
 
-NSTEST_ADD(test_ns_exception_ctor)
+NSTEST_ADD(test_ns_exception)
