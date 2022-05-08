ab6d756bad1dcd854f18788360593c3f7bdfbfa2
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-05-08 18:04:20 +0300

    _test_exception.cpp: new tests

--- src/core/exception/_test_exception.cpp
+++ src/core/exception/_test_exception.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace nanoservices;
 
-static const function<bool()> test_ns_exception = []() {
+static const function<bool()> test_ns_exception_whatstr = []() {
     auto e1 = make_shared<ns_exception>("My exception 1", NS_POSITION_SHAREDPTR);
     auto e2 = make_shared<ns_exception>(e1, make_shared<string>("My exception 2"), NS_POSITION_SHAREDPTR);
 
@@ -35,4 +35,48 @@ static const function<bool()> test_ns_exception = []() {
     return result;
 };
 
-NSTEST_ADD(test_ns_exception)
+NSTEST_ADD(test_ns_exception_whatstr)
+
+static const function<bool()> test_ns_exception_message = []() {
+    const char *msg = "My exception";
+
+    auto e = make_shared<ns_exception>(msg, NS_POSITION_SHAREDPTR);
+
+    bool result = true;
+    if(e->message()->compare(msg) != 0) {
+        log::warn("e->message(): \"{}\" is not the expected \"{}\"", e->message(), msg);
+        result = false;
+    }
+    return result;
+};
+
+NSTEST_ADD(test_ns_exception_message)
+
+static const function<bool()> test_ns_exception_position = []() {
+    auto e = make_shared<ns_exception>("My exception", NS_POSITION_SHAREDPTR);
+    bool result = true;
+
+    auto position = e->position();
+    regex re("[a-zA-Z0-9\\/_.]*:[0-9]*, in <lambda\\(\\)>");
+    if(!regex_match(*position, re)) {
+        log::warn("position: {} does not match regular expression");
+        result = false;
+    }
+    return result;
+};
+
+NSTEST_ADD(test_ns_exception_position)
+
+static const function<bool()> test_ns_exception_cause = []() {
+    ns_exception e2 = []() {
+        ns_exception e1("My exception", NS_POSITION_SHAREDPTR);
+        ns_exception e2(e1, make_shared<string>("Another exception"), NS_POSITION_SHAREDPTR);
+        return e2;
+    }();
+
+    auto e1ptr = e2.cause(); // If no segfault here, then everything is ok.
+
+    return true;
+};
+
+NSTEST_ADD(test_ns_exception_cause)
