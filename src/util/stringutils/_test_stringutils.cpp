283f9bfd4685f7237cf3622bab6de8959436ade3
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-05-09 02:42:13 +0300

    _test_stringutils.cpp: refactor test_strutils_splitString()

--- src/util/stringutils/_test_stringutils.cpp
+++ src/util/stringutils/_test_stringutils.cpp
@@ -1,3 +1,4 @@
+#include "../../core/exception/ns_exception.h"
 #include "../../plugins/logging/logging.h"
 #include "../testing/testing.h"
 #include "stringutils.h"
@@ -14,11 +15,6 @@ static const std::function<bool()> test_strutils_fmt_sharedptrstring_formatter =
 NSTEST_ADD(test_strutils_fmt_sharedptrstring_formatter)
 
 static const std::function<bool()> test_strutils_splitString = []() {
-    auto str = make_shared<string>("My String  is quite long, and good enough");
-
-    bool result = true;
-    log::debug("Input string: \"{}\"", str);
-
     auto check_against = [](vector<string> check, shared_ptr<vector<shared_ptr<string>>> in) {
         if(check.size() != in->size()) {
             return false;
@@ -32,7 +28,9 @@ static const std::function<bool()> test_strutils_splitString = []() {
         return true;
     };
 
-    auto check = [&str, &result, &check_against](char delimiter, bool trimTokens, bool dropEmptyTokens, vector<string> check) {
+    auto str = make_shared<string>("My String  is quite long, and good enough");
+
+    auto check = [&str, &check_against](char delimiter, bool trimTokens, bool dropEmptyTokens, vector<string> check) {
         auto split = splitString(str, delimiter, trimTokens, dropEmptyTokens);
         auto check_result = check_against(check, split);
         log::debug("delimiter: '{}' trimTokens: {:<5} dropEmptyTokens: {:<5} check: {:<65} -> {:<65}: {}",
@@ -42,13 +40,17 @@ static const std::function<bool()> test_strutils_splitString = []() {
                    fmt::format("{}", check),
                    fmt::format("{}", *split),
                    check_result ? "OK" : "FAILED");
-        result = result & check_result;
+        return check_result;
     };
 
-    check(' ', false, true, {"My", "String", "is", "quite", "long,", "and", "good", "enough"});
-    check(' ', false, false, {"My", "String", "", "is", "quite", "long,", "and", "good", "enough"});
-    check(',', false, true, {"My String  is quite long", " and good enough"});
-    check(',', true, true, {"My String  is quite long", "and good enough"});
+    log::debug("Input string: \"{}\"", str);
+
+    bool result = true;
+
+    result &= check(' ', false, true, {"My", "String", "is", "quite", "long,", "and", "good", "enough"});
+    result &= check(' ', false, false, {"My", "String", "", "is", "quite", "long,", "and", "good", "enough"});
+    result &= check(',', false, true, {"My String  is quite long", " and good enough"});
+    result &= check(',', true, true, {"My String  is quite long", "and good enough"});
 
     return result;
 };
